51ea1258d322362f9eee4c81108199a470430a01
yangjasp <61026656+yangjasp@users.noreply.github.com>
2025-12-22 17:57:15 -0500

    Add authorship and reproducibility comments
    Added credit and reproducibility notes for the script.

--- ossat_functionsMarksAnglin2025.R
+++ ossat_functionsMarksAnglin2025.R
@@ -1,3 +1,7 @@
+## This script is from the supplemental material of Marks-Anglin et al. (2025). 
+## It is not original to the present manuscript and is included solely to ensure reproducibility of the analyses.
+## All credit for this code belongs to the original authors.
+
 ## Marks-Anglin, et al. 2024. Optimal surrogate-assisted sampling for cost-efficient validation of electronic health record outcomes
 ## simulation functions for the novel method OSSAT (Optimal Subsampling strategy with Surrogate-Assisted Two-step procedure)
 
